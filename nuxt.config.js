723d6592382d4d539931d827943adf2be034522d
Aleksandr Shilov <alex.shilov.by@gmail.com>
2022-02-25 00:13:28 +0300

    Change viewport
    Signed-off-by: Aleksandr Shilov <alex.shilov.by@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     },
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'},
       {hid: 'description', name: 'description', content: ''},
       {name: 'format-detection', content: 'telephone=no'},
       {name: "msapplication-TileColor", content: "#ff9900"},

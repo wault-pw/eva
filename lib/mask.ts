3500ede81351440e00c82088eb9c34aa9510588b
Aleksandr Shilov <alex.shilov.by@gmail.com>
2022-03-20 22:19:39 +0300

    Change asterisks mask char for working in mobile webkit
    Signed-off-by: Aleksandr Shilov <alex.shilov.by@gmail.com>

--- lib/mask.ts
+++ lib/mask.ts
@@ -1,3 +1,6 @@
 export function Mask(input: string | null): string {
-  return input == null ? "" : input.replace(/./g, '*')
+  // with regular asterisks mask char "*", css word-reak:break-all;
+  // property not working on mobile safari and chrome
+  // @refs https://unicode-table.com/en/sets/star-symbols/
+  return input == null ? "" : input.replace(/./g, '✶')
 }
